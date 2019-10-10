1cc33d7f3b56ae6a81aa0a00ca8f59c49fd3b4a7
sdonich <sdonich@gmail.com>
2019-10-10 21:16:33 +0200

    Add htmlReport component

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import {ConsoleReport} from './reportTargets/ConsoleReports';
-import {WinsAnalysis} from './analyzers/WinsAnalysis';
-import {Summary} from './Summary';
+// import { ConsoleReport } from './reportTargets/ConsoleReports';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
+// const summary = new Summary(
+//   new WinsAnalysis('Man United'),
+//   new ConsoleReport()
+// );
+
 const summary = new Summary(
   new WinsAnalysis('Man United'),
-  new ConsoleReport
+  new HtmlReport()
 );
 
 summary.buildAndPrintReport(matchReader.matches);
-
