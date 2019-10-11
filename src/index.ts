61bc01edae63ae5da93f8310fb7e25265ef4cd4b
Sergei <sdonich@gmail.com>
2019-10-11 11:50:07 +0200

    Fin

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-// import { ConsoleReport } from './reportTargets/ConsoleReports';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
+// import { CsvFileReader } from './CsvFileReader';
+// import { ConsoleReport } from './reportTargets/ConsoleReports';
+// import { WinsAnalysis } from './analyzers/WinsAnalysis';
+// import { HtmlReport } from './reportTargets/HtmlReport';
 
-const csvFileReader = new CsvFileReader('football.csv');
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
+// varian of implementation without calling a static method of Summary
+// const csvFileReader = new CsvFileReader('football.csv');
+// const matchReader = new MatchReader(csvFileReader);
 // const summary = new Summary(
 //   new WinsAnalysis('Man United'),
 //   new ConsoleReport()
 // );
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new HtmlReport()
-);
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
